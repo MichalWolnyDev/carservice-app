6144f62f208661e898fb0ba19e8cdfefbed8b7c1
MichalWolnyDev <mwolny98@interia.pl>
2022-07-17 12:25:01 +0200

    add titles

--- src/views/Pojazdy.vue
+++ src/views/Pojazdy.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
-   test
+    <div class="">
+      <h2 class="title title__h2 text__center">Moje pojazdy</h2>
+    </div>
   </div>
 </template>
--- src/views/Profil.vue
+++ src/views/Profil.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
-   test
+   <div class="profile">
+      <h2 class="title title__h2 text__center">Witaj! XYZ</h2>
+   </div>
   </div>
 </template>
