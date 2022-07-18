733dd79a46f447304ff4d5a6d5d3d24ee961f9fe
Wojciech Nowakowski <kokoshkv@gmail.com>
2022-07-18 21:03:09 +0200

    dodanie wstępnego widoku

--- src/views/Profil.vue
+++ src/views/Profil.vue
@@ -2,6 +2,52 @@
   <div>
    <div class="profile">
       <h2 class="title title__h2 text__center">Witaj! XYZ</h2>
+        <div class="profile_title">
+            Informacje o użytkowniku
+        </div>
+        <table class="profile_table"> 
+          <tr>
+           <th>Imię:</th>
+           <td>XYZ</td>
+          </tr>
+          <tr>
+            <th>Nazwisko:</th>
+            <td>XYZ</td>
+          </tr>
+          <tr>
+            <th>E-mail:</th>
+            <td>xyz@abc.com</td>
+          </tr>
+          <tr>
+            <th>Twoja rola w serwisie:</th>
+            <td class="profile_admin">ADMIN</td>
+          </tr>
+        </table>
+       
    </div>
   </div>
 </template>
+<style lang="scss">
+.profile{
+  &_title{
+    margin: 30px 0;
+    font-size: 20px;
+  }
+  &_table{
+    text-align:left;
+  }
+  th{
+    font-weight:bold;
+  }
+  &_admin{
+    color:red;
+  }
+  &_user{
+    color:green;
+  }
+  &_employee{
+    color:orange;
+  }
+}
+
+</style>
