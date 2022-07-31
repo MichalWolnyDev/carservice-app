3c6ac1637d4a3d77168ece20fa1c8775d0fe9721
MichalWolnyDev <mwolny98@interia.pl>
2022-07-31 11:11:05 +0200

    fk it

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 import Vuelidate from "vuelidate";
 
-// axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
+axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
 
 Vue.use(Vuelidate)
 Vue.use(VCalendar, {
