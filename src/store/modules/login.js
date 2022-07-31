cf51bf663ec0dd89a25a0d5828faf49026b735d1
MichalWolnyDev <mwolny98@interia.pl>
2022-07-31 11:29:22 +0200

    asmddamd

--- src/store/modules/login.js
+++ src/store/modules/login.js
@@ -38,8 +38,7 @@ export default {
             headers: {
               "Content-Type": "application/json",
               "Access-Control-Allow-Origin": "*",
-              "Access-Control-Allow-Credentials": true,
-              "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization, authorization",
+              "Access-Control-Allow-Headers": "Authorization",
 
             },
           }
