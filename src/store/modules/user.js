9d0ed21b16622a3fe58448283c79cc1753ded289
MichalWolnyDev <mwolny98@interia.pl>
2022-07-31 12:01:01 +0200

    ochujtuchodzi

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -33,7 +33,7 @@ export default {
             "Content-Type": "application/json",
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Credentials": true,
-            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
             "Authorization": `Bearer ${token}`
           }
         })
