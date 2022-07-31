226c70c4605d3c6b9b484fea28e0aa7fcd35154c
MichalWolnyDev <mwolny98@interia.pl>
2022-07-31 11:52:17 +0200

    jebac to wszystko

--- src/store/modules/login.js
+++ src/store/modules/login.js
@@ -38,7 +38,7 @@ export default {
             headers: {
               "Content-Type": "application/json;charset=utf-8",
               "Access-Control-Allow-Origin": "*",
-              "Access-Control-Allow-Headers": "Content-Type, Authorization",
+              "Access-Control-Allow-Headers": "Content-Type",
               "Access-Control-Allow-Methods": "*"
 
             },
