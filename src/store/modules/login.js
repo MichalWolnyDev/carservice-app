2fbb8d22b6c9865a5bbdefd20a31dac55ab0f695
MichalWolnyDev <mwolny98@interia.pl>
2022-07-31 13:06:30 +0200

    asd

--- src/store/modules/login.js
+++ src/store/modules/login.js
@@ -36,8 +36,7 @@ export default {
           },
           {
             headers: {
-              "Content-Type": "application/json",
-              "Access-Control-Allow-Origin": "*"
+              "Access-Control-Allow-Origin": "*",
             },
           }
         )
