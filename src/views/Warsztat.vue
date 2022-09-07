ea39e9c27c88c58c33e292191dfcc9b6a5016243
Wojciech Nowakowski <kokoshkv@gmail.com>
2022-09-07 23:50:32 +0200

    uzupełnienie mój warsztat

--- src/views/Warsztat.vue
+++ src/views/Warsztat.vue
@@ -1,6 +1,62 @@
 <template>
   <div>
     <h2 class="title title__h2 text__center">Mój warsztat (dla właściciela)</h2>
-   
+    <div class="garage">
+      <div class="garage__title">Informacje o warsztacie</div>
+      <table class="garage__table">
+        <tr>
+          <th>Nazwa serwisu:</th>
+          <td>AutoMech - Mechanik</td>
+        </tr>
+        <tr>
+          <th>Adres:</th>
+          <td>ul. Kolorowa 20/2
+            60-001 Poznań
+          </td>
+        </tr>
+        <tr>
+          <th>Godziny otwarcia:</th>
+           <ul>
+              <li>Pn - Pt: 8:00 - 20:00 </li>
+              <li>Sobota: nieczynne </li>
+              <li>Niedziela: nieczynne </li>
+           </ul>
+        </tr>
+        <tr>
+          <th>Oferowane usługi:</th>
+          <td>
+            <ul>
+              <li>Sprawdzenie samochodu przed zakupem</li>
+              <li>Serwis klimatyzacji</li>
+              <li>Poprawki lakiernicze</li>
+            </ul>
+          </td>
+        </tr>
+      </table>
+    </div>
+    <p class="garage__message">Nie posiadasz jeszcze swojego warsztatu!</p>
   </div>
+ 
 </template>
+<style lang="scss">
+  .garage {
+    &__title {
+      margin: 30px 0;
+      font-size: 20px;
+    }
+    &__table {
+      text-align: left;
+    }
+    &__message{
+      margin: auto;
+      width: 40%;
+      font-size: 20px;
+    }
+    th {
+      font-weight: bold;
+    }
+    li {
+      text-align: left;
+    }
+  }
+  </style>
