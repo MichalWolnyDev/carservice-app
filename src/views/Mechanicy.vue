14364eab5affaf0315e4cf3ee20fb48a75398f2a
Wojciech Nowakowski <kokoshkv@gmail.com>
2022-09-07 22:44:07 +0200

    dodanie listingu mechaników

--- src/views/Mechanicy.vue
+++ src/views/Mechanicy.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
     <h2 class="title title__h2 text__center">Mechanicy (dla właściciela)</h2>
-   
+     <div><ListingMechanics/></div>
   </div>
+    
 </template>
+<script>
+  import ListingMechanics from "@/components/Listing/ListingMechanics.vue";
+
+  
+  export default {
+    components: {
+      ListingMechanics,
+    },
+  };
+  </script>
