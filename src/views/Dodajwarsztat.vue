45611c259b9fe9f2e5e7dfb894e423f9f00544fa
Wojciech Nowakowski <kokoshkv@gmail.com>
2022-09-07 22:49:36 +0200

    dodanie przycisku dodaj warsztat xD

--- src/views/Dodajwarsztat.vue
+++ src/views/Dodajwarsztat.vue
@@ -1,6 +1,27 @@
 <template>
   <div>
     <h2 class="title title__h2 text__center">Dodaj warsztat (dla właściciela)</h2>
-   
+    <div class="cars">
+      <div class="cars__top">
+        <Button :green="true"> Dodaj warsztat </Button>
+      </div>
+    </div>
   </div>
 </template>
+<script>
+  import Button from "@/components/Inputs/Button.vue";
+ 
+  export default {
+    components: {
+      Button
+    }
+  };
+  </script>
+  <style lang="scss">
+  .cars{
+  
+    &__top {
+      margin: 30px 0;
+    }
+  }
+  </style>
