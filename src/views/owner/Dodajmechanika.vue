b42a039b44ae11ac4e02d58b9da826f4e30bbce2
MichalWolnyDev <mwolny98@interia.pl>
2022-09-25 10:50:07 +0200

    addmechanic POST but not working yet

--- src/views/owner/Dodajmechanika.vue
+++ src/views/owner/Dodajmechanika.vue
@@ -1,47 +1,42 @@
 <template>
   <div>
-    <h2 class="title title__h2 text__center">Dodaj mechaników (dla właściciela)</h2>
+    <h2 class="title title__h2 text__center">
+      Dodaj mechaników (dla właściciela)
+    </h2>
     <div class="addMechanic">
       <div class="addMechanic__form">
         <div class="addMechanic__form-wrap">
-            <div class="addMechanic__form-item">
-              <Input
-                 type="text"
-                v-model.trim="$v.formData.email.$model"
-                :error="$v.formData.email.$error"
-              >
-                <template> Adres e-Mail </template>
-              </Input>
-              <p class="addMechanic__form-error" v-if="$v.formData.email.$error">
-                To pole jest wymagane
-              </p>
-            </div>  
-            <div class="addMechanic__top">
-            <Button :green="true"
-            @click.native.prevent="confirmMechanicAdd"
-            v-if="!showLoader"
-          >
-            Dodaj mechanika
-          </Button>
-         
-        </div>  
+          <div class="addMechanic__form-item">
+            <Input
+              type="text"
+              v-model.trim="$v.formData.email.$model"
+              :error="$v.formData.email.$error"
+            >
+              <template> Adres e-Mail </template>
+            </Input>
+            <p class="addMechanic__form-error" v-if="$v.formData.email.$error">
+              To pole jest wymagane
+            </p>
+          </div>
+          <div class="addMechanic__top">
+            <Button
+              :green="true"
+              @click.native.prevent="confirmMechanicAdd"
+              v-if="!showLoader"
+            >
+              Dodaj mechanika
+            </Button>
+          </div>
         </div>
-            </div>
-          
-
       </div>
-
     </div>
- 
+  </div>
 </template>
 <script>
-
 import Input from "@/components/Inputs/Input.vue";
 import Button from "@/components/Inputs/Button.vue";
 
-
 import { required, email } from "vuelidate/lib/validators";
-
 
 export default {
   components: {
@@ -52,7 +47,7 @@ export default {
   data() {
     return {
       formData: {
-      email: "",
+        email: "",
       },
       resetFormData: {
         email: "",
@@ -75,47 +70,67 @@ export default {
         console.log("error");
         this.showLoader = false;
         return;
-      } 
-      else {
+      } else {
         console.log("nie ma błędu");
       }
-    
-  }}}
+    },
+    async addMechanicRole() {
+      const BASE_URL = process.env.VUE_APP_BASEURL;
+      let token = localStorage.getItem("token")
 
+      await this.$axios
+        .post(
+          BASE_URL + "/mechanic",
+          {
+            mechanicEmail: this.formData.email
+          },
+          {
+            headers: { Authorization: `Bearer ${token}` }
+          }
+        )
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-  .addMechanic {
-    &__top {
-      margin: 30px 0;
+.addMechanic {
+  &__top {
+    margin: 30px 0;
+  }
+  &__form {
+    margin-top: 15px;
+    &-label {
+      font-size: 14px;
+      margin-bottom: 10px;
     }
-    &__form {
-      margin-top: 15px;
-      &-label {
+    &-service {
+      span {
         font-size: 14px;
-        margin-bottom: 10px;
       }
-      &-service {
-        span {
-          font-size: 14px;
-        }
+    }
+    &-item {
+      width: calc(100% / 4 - 1rem);
+
+      @media (max-width: 960px) {
+        width: calc(100% / 2 - 1rem);
       }
-      &-item {
-        width: calc(100% / 4 - 1rem);
-  
-        @media (max-width: 960px) {
-          width: calc(100% / 2 - 1rem);
-        }
-        @media (max-width: 600px) {
-          width: calc(100% / 1 - 1rem);
-        }
+      @media (max-width: 600px) {
+        width: calc(100% / 1 - 1rem);
       }
-      &-error {
-        color: $redError;
-        font-size: 0.6rem;
-        margin: 0;
-      }
+    }
+    &-error {
+      color: $redError;
+      font-size: 0.6rem;
+      margin: 0;
     }
   }
-  </style>
+}
+</style>
   
