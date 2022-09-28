131c3ed8bf497175b1b2a3db1384296ef8b15cb4
MichalWolnyDev <mwolny98@interia.pl>
2022-09-28 11:12:16 +0200

    addmechanic

--- src/views/owner/Dodajmechanika.vue
+++ src/views/owner/Dodajmechanika.vue
@@ -18,26 +18,47 @@
               To pole jest wymagane
             </p>
             <CustomSelect
-            :options="getOwnedGarages"
-            :default="'wybierz'"
-            class="select"
-            @input="
-              fetchModels($event.name);
-              form.brand = $event.name;
-            "
-          >
-            <template> Warsztat </template>
-          </CustomSelect>
+              :options="getOwnedGarages"
+              :default="'wybierz'"
+              class="select"
+              :isError="$v.formData.garageId.$error"
+              @input="formData.garageId = $event.id"
+            >
+              <template> Warsztat </template>
+            </CustomSelect>
+            <p
+              class="addMechanic__form-error"
+              v-if="$v.formData.garageId.$error"
+            >
+              To pole jest wymagane
+            </p>
           </div>
-          <div class="addMechanic__top">
+            <div class="addMechanic__error" v-if="isAxiosError">
+            <h3>
+              {{ errorMsg }}
+            </h3>
+          </div>
+          <div class="addMechanic__top" v-if="!success">
             <Button
               :green="true"
               @click.native.prevent="confirmMechanicAdd"
               v-if="!showLoader"
+
             >
               Dodaj mechanika
             </Button>
+            <Loader v-else />
+
           </div>
+          <!-- <div class="addMechanic__top" v-else>
+            <Button :green="true" @click.native.prevent="resetForm">
+              Dodaj kolejnego mechanika
+            </Button>
+          </div> -->
+          <div class="addMechanic__success" v-if="success">
+            <h3>Mechanik został poprawnie dodany</h3>
+          </div>
+        
         </div>
       </div>
     </div>
@@ -47,39 +68,52 @@
 import Input from "@/components/Inputs/Input.vue";
 import Button from "@/components/Inputs/Button.vue";
 import CustomSelect from "@/components/Inputs/CustomSelect.vue";
+import Loader from "@/components/Loader.vue";
 import { required, email } from "vuelidate/lib/validators";
-import search from "@/mixins/search"
+import search from "@/mixins/search";
 
 export default {
   components: {
     Input,
     Button,
     CustomSelect,
+    Loader
   },
-  name: 'Warsztat',
+  name: "Warsztat",
   mixins: [search],
   mounted() {
-    this.fetchOwnedGarages()
+    this.fetchOwnedGarages();
   },
-  
+
   data() {
     return {
       formData: {
         email: "",
+        garageId: null,
       },
       resetFormData: {
         email: "",
+        garageId: null,
       },
       filteredCities: [],
       showLoader: false,
+      success: false,
+      isAxiosError: false,
+      errorMsg: "",
     };
   },
   validations: {
     formData: {
       email: { required, email },
+      garageId: { required },
     },
   },
   methods: {
+    resetForm() {
+      this.$v.formData.$reset;
+      this.formData = this.resetFormData;
+      this.success = false;
+    },
     confirmMechanicAdd() {
       this.$v.$touch();
       this.showLoader = true;
@@ -89,28 +123,38 @@ export default {
         this.showLoader = false;
         return;
       } else {
-        console.log("nie ma błędu");
+        console.log("nie ma błędu walidacji");
+        this.addMechanicRole();
       }
     },
     async addMechanicRole() {
       const BASE_URL = process.env.VUE_APP_BASEURL;
-      let token = localStorage.getItem("token")
+      let token = localStorage.getItem("token");
 
       await this.$axios
         .post(
           BASE_URL + "/mechanic",
           {
-            mechanicEmail: this.formData.email
+            mechanicEmail: this.formData.email,
+            garageId: this.formData.garageId,
           },
           {
-            headers: { Authorization: `Bearer ${token}` }
+            headers: { Authorization: `Bearer ${token}` },
           }
         )
         .then((res) => {
           console.log(res);
+          if (res.status == 200) {
+            this.isAxiosError = false;
+            this.success = true;
+            this.showLoader = false;
+          }
         })
         .catch((err) => {
           console.log(err);
+          this.isAxiosError = true;
+          this.errorMsg = err.response.data;
+          this.showLoader = false;
         });
     },
   },
@@ -147,6 +191,17 @@ export default {
       color: $redError;
       font-size: 0.6rem;
       margin: 0;
+    }
+  }
+
+  &__success {
+    h3 {
+      color: $green;
+    }
+  }
+  &__error {
+    h3 {
+      color: $redError;
     }
   }
 }
