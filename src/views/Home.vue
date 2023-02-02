9b0866ee747ffad99b2f8e5e8e2a4fc6c7e0937a
MichalWolnyDev <mwolny98@interia.pl>
2023-02-02 19:40:26 +0100

    home videos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,46 +1,70 @@
 <template>
   <div class="home">
-    <!-- <h2 class="title title__h2 text__center">Strona główna</h2>
-     <div class="reservation__title">Aktualne rezerwacje</div>
-    <table class="reservation__table">
-        <tr>
-          <th>Pojazd</th>
-          <th>Termin odstawienia pojazdu</th>
-          <th>Termin oddania pojazdu</th>
-          <th>Szczegóły</th>
-        </tr>
-        <tr>
-          <td>Volskwagen Passat</td>
-          <td>22.09.2022</td>
-          <td>23.09.2022</td>
-          <td>Wymiana filtrów</td>
-        </tr>
-        <tr>
-          <td>Opel Omega</td>
-          <td>26.09.2022</td>
-          <td>26.09.2022</td>
-          <td>Wymiana opon</td>
-        </tr>
-      </table>
-      <div class="reservation__title">Uwagi</div>
-      <div class="reservation__note">Dnia 05.10.2022 pojazd Volkswagen Passat utraci ważność przeglądu technicznego.</div> -->
+    <div class="home__wrap">
+      <div class="home__header">
+        <h1 class="home__title">
+          Witaj! <span class="home__name">{{ getUserInfo.firstName }} {{ getUserInfo.lastName }}</span>
+        </h1>
+        <p class="home__description">
+          Prosimy o zapoznanie się z wideoinstrukcją obsługi systemu.
+        </p>
+      </div>
+      <div class="home__video-wrap">
+        <div class="home__videobox" v-if="getUserInfo.authorities[0] == 'USER'">
+          <video class="home__video" controls>
+            <source src="@/assets/video/user.mp4" type="video/mp4" />
+          </video>
+        </div>
+        <div class="home__videobox" v-if="getUserInfo.authorities[0] == 'OWNER'">
+          <video class="home__video" controls>
+            <source src="@/assets/video/owner.mp4" type="video/mp4" />
+          </video>
+        </div>
+        <div class="home__videobox" v-if="getUserInfo.authorities[0] == 'MECHANIC'">
+          <video class="home__video" controls>
+            <source src="@/assets/video/mechanic.mp4" type="video/mp4" />
+          </video>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import search from "@/mixins/search";
 
 export default {
   name: "Home",
+  mixins: [search],
 
-
- 
-  // components: {
-  // }
+  mounted() {
+    this.fetchUserInfo();
+  },
 };
 </script>
 
 <style lang="scss">
+.home {
+  &__title {
+    text-align: center;
+    margin: 0 0 30px;
+  }
+  &__video{
+    width: 100%;
+  }
+  &__description{
+    text-align: center;
+    margin: 0 0 15px;
+  }
+  &__videobox{
+        max-width: 768px;
+    margin: 0 auto;
+  }
+  &__name{
+    text-transform: capitalize;
+  }
+}
 .reservation {
   &__title {
     margin: 30px 0;
